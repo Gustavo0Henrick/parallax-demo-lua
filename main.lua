8169695a9535e537325237399ad7e89414e88c8c
Gustavo Henrick <gustavo08henrick@gmail.com>
2021-12-08 11:16:17 -0300

    fix sound

--- main.lua
+++ main.lua
@@ -6,14 +6,14 @@ require('Parallax')
 function love.load()
 	love.window.setFullscreen(true)
 	Parallax:load()
-	src = love.audio.newSource("assets/sounds/music.ogg", "stream")
-	src:setVolume(0.7)
+	Src = love.audio.newSource("assets/sounds/music.ogg", "stream")
+	Src:setVolume(0.7)
 end
 
 
 function love.update( dt )
-	if not src:isPlaying() then
-		love.audio.play(src)
+	if not Src:isPlaying() then
+		love.audio.play(Src)
 	end
 	if love.keyboard.isDown("escape") then
 		love.event.quit()
